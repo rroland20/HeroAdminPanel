4d49549052ad7d0c0994c80f99e2e7f35c4f4e9e
rroland20 <alena_tumanova96@mail.ru>
2024-03-19 18:46:50 +0400

    add createSelector

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -2,6 +2,7 @@ import {useHttp} from '../../hooks/http.hook';
 import { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { CSSTransition, TransitionGroup} from 'react-transition-group';
+import { createSelector } from 'reselect';
 
 import { heroesFetching, heroesFetched, heroesFetchingError, heroesDelete } from '../../actions';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
@@ -10,14 +11,27 @@ import Spinner from '../spinner/Spinner';
 import './heroesList.scss';
 
 const HeroesList = () => {
-    const filteredHeroes = useSelector(state => {
-        if (state.filters.activeFilter === 'all') {
-            return state.heroes.heroes;
+    const filteredHeroesSelector = createSelector(
+        (state) => state.filters.activeFilter,
+        (state) => state.heroes.heroes,
+        (filter, heroes) => {
+            if (filter === 'all') {
+                return heroes;
+            }
+            else {
+                return heroes.filter(item => item.element === filter)
+            }
         }
-        else {
-            return state.heroes.heroes.filter(item => item.element === state.filters.activeFilter)
-        }
-    });
+    );
+    const filteredHeroes = useSelector(filteredHeroesSelector)
+    // const filteredHeroes = useSelector(state => {
+    //     if (state.filters.activeFilter === 'all') {
+    //         return state.heroes.heroes;
+    //     }
+    //     else {
+    //         return state.heroes.heroes.filter(item => item.element === state.filters.activeFilter)
+    //     }
+    // });
     const heroesLoadingStatus = useSelector(state => state.heroesLoadingStatus);
     const dispatch = useDispatch();
     const {request} = useHttp();
